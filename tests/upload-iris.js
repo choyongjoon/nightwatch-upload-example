f985e58526fc7de74672ea22fac5ab3f97380ba2
Yongjoon Cho <yjcho@solidware.io>
2017-03-06 17:53:41 +0900

    Add file-size div

--- tests/upload-iris.js
+++ tests/upload-iris.js
@@ -4,9 +4,11 @@ const filePath = require('path').resolve(__dirname, fileName)
 module.exports = {
   'Upload iris': client => {
     client
-      .url('localhost:8080')
+      .url('https://choyongjoon.github.io/nightwatch-upload-example/')
       .waitForElementPresent('input#file', 5000)
       .setValue('input#file', filePath)
+      .waitForElementPresent('#file-size', 5000)
+      .assert.containsText('#file-size', '0') // 4630
       .end()
   }
 }
